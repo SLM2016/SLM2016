ff234a7b850b09da4d43dda76d0f6a21201b39a4
WindAzure <cvbnm0002@gmail.com>
2016-04-08 15:44:22 +0800

    input text on forward side, and use post command

--- SLM2016/WebContent/js/certificationPageJS.js
+++ SLM2016/WebContent/js/certificationPageJS.js
@@ -1,17 +1,13 @@
 function ClickGenerateButton() {
-	$.get("http://localhost:8080/SLM2016/CertificationServlet", function(
-			responseText) {
-		document.getElementById('someImg').setAttribute('src',
-				'data:image/png;base64,' + responseText);
-
+    var name = document.getElementById('studentName').value;
+    var id = document.getElementById('certificationId').value;
+	var data=new Object();
+	data.id_=id;
+	data.owner_=name;
+	
+	$.post("http://localhost:8080/SLM2016/CertificationServlet",JSON.stringify(data))
+	.done(function(data)
+	{
+		document.getElementById("someImg").setAttribute('src','data:image/png;base64,'+data);
 	});
-	document.getElementById('lastButton').style.display = "inline";
-	document.getElementById('nextButton').style.display = "inline";
-	document.getElementById('generateButton').style.display = "none";
-}
-
-function ClickLastButton() {
-}
-
-function ClickNextButton() {
 }
