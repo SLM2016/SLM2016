cb86e05e3fc5b05e3be774054c6357f27f6d658a
Brian <tp26610@gamil.com>
2016-05-16 22:01:24 +0800

    add email context

--- SLM2016/WebContent/js/controllers/studentSendmail.js
+++ SLM2016/WebContent/js/controllers/studentSendmail.js
@@ -1,6 +1,19 @@
 app.controller('StudentSendmailController', ['$scope', '$state', '$stateParams', '$timeout', '$rootScope', 'StudentInfoService',  
     function ($scope, $state, $stateParams, $timeout, $rootScope, StudentInfoService) {  
-
+	
+		var Sendcontext =function(){
+			console.debug(tinyMCE.activeEditor.getContent());
+	  	    var i=tinyMCE.get("con").getContent();
+	  	    var g=tinyMCE.activeEditor.getContent({format : 'raw'});
+	  	      var saveData =
+  	    	$.ajax({url: "http://httpbin.org/post",
+		  	        type: "POST",
+		  	        data: { emailcontent:g, method: "example"},
+		  	        dataType: "json"});
+	  	    
+	  	    
+	  	}
+	  
 		var setValue = function(){ 		
 			courseName.value = parse[index].courseName;
     		studentName.value = parse[index].studentName;
@@ -9,7 +22,8 @@ app.controller('StudentSendmailController', ['$scope', '$state', '$stateParams',
     		couresDuration.value = parse[index].couresDuration;
 		}
 		
-		var ClickNextButton = function(){			
+		var ClickNextButton = function(){	
+			console.debug(tinyMCE.activeEditor.getContent());
 			if(++index <= parse.length-1){	
 				previousButton.disabled = "";
 				courseName.value = parse[index].courseName;
@@ -43,6 +57,10 @@ app.controller('StudentSendmailController', ['$scope', '$state', '$stateParams',
     		if(parse.length-1 > 0){
     			nextButton.disabled = "";
     		}
+    		tinymce.init({  selector:'textarea',
+			    			theme:"modern",
+			    			plugins: [" autolink link"],
+			    			menubar: false,toolbar: "link" });
         }
 
 		/*==========================
@@ -60,7 +78,7 @@ app.controller('StudentSendmailController', ['$scope', '$state', '$stateParams',
         /*==========================
              init
         ==========================*/
-   	
+ 
     	var parse;
     	var index = 0;
     	var courseName = document.getElementById("courseName");	
@@ -72,6 +90,10 @@ app.controller('StudentSendmailController', ['$scope', '$state', '$stateParams',
     	var nextButton = document.getElementById("next");
     	var previousButton = document.getElementById("previous");
     	
+    	var sendButton = document.getElementById("send");
+    	
+    	
+    	$scope.Sendcontext = Sendcontext;
     	$scope.ClickNextButton = ClickNextButton;
     	$scope.ClickPreviousButton = ClickPreviousButton;
     	
